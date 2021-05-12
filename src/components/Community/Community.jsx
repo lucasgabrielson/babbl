1534a6aaaeb6921501beca1c494c3b986d9b3d79
gabinwilliams <scoutsnknives@gmail.com>
2021-05-12 16:57:54 -0500

    preparing push

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import TrendingNews from './TrendingNews/TrendingNews'
+import TrendingNews from './TrendingNews/TrendingNews'
 
 import Tabs from '../Community/Tabs/Tabs';
 
