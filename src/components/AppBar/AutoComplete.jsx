5eaf619bb3719ebac365877dbe3c24a585511d53
lucasgabrielson <lucas.c.gabrielson@gmail.com>
2021-05-18 11:53:07 -0500

    added latch to autocomplete

--- src/components/AppBar/AutoComplete.jsx
+++ src/components/AppBar/AutoComplete.jsx
@@ -18,14 +18,19 @@ const useStyles = makeStyles({
 
 export default function TickerSelect() {
     const classes = useStyles();
-    const tickers = useSelector( store => store.populated_tickers.meta.all_tickers)
+    let tickers = useSelector( store => store.populated_tickers)
+    let latch = false
     const [search, setSearch] = useState('');
     const history = useHistory();
 
+    if( !latch && tickers.meta !== undefined) {
+        latch = true;
+        tickers = tickers.meta.all_tickers;
+    }
     // const countries = [
     // { code: 'AD', label: 'Andorra', phone: '376' },
 
-    // const tickers = []
+    // let tickers = []
 
     const handleChange = (value) => {
         history.push(`/company/${value}`, {params: value})
@@ -33,31 +38,31 @@ export default function TickerSelect() {
 
     return (
         <Autocomplete
-        id="ticker-select"
-        style={{ width: 300 }}
-        options={tickers}
-        classes={{
-            option: classes.option,
-        }}
-        autoHighlight
-        onChange={(event, value) => handleChange(value)}
-        getOptionLabel={(option) => option}
-        renderOption={(option) => (
-            <React.Fragment>
-                {option}
-            </React.Fragment>
-        )}
-        renderInput={(params) => (
-            <TextField
-            {...params}
-            label="Choose a ticker"
-            variant="outlined"
-            inputProps={{
-                ...params.inputProps,
-                autoComplete: 'new-password', // disable autocomplete and autofill
+            id="ticker-select"
+            style={{ width: 300 }}
+            options={tickers}
+            classes={{
+                option: classes.option,
             }}
-            />
-        )}
+            autoHighlight
+            onChange={(event, value) => handleChange(value)}
+            getOptionLabel={(option) => option}
+            renderOption={(option) => (
+                <React.Fragment>
+                    {option}
+                </React.Fragment>
+            )}
+            renderInput={(params) => (
+                <TextField
+                {...params}
+                label="Choose a ticker"
+                variant="outlined"
+                inputProps={{
+                    ...params.inputProps,
+                    autoComplete: 'new-password', // disable autocomplete and autofill
+                }}
+                />
+            )}
         />
     );
 }
