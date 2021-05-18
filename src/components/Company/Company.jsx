bb2db5f07a2ee3278c0e5437ebaf023f867f9ce4
Trace Brandt <trpbr92@gmail.com>
2021-05-18 16:17:50 -0500

    company page overhaul

--- src/components/Company/Company.jsx
+++ src/components/Company/Company.jsx
@@ -1,85 +1,26 @@
-import React from 'react';
+import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
+
+import Overview from '../Company/Overview/Overview';
 
 
-import Articles from '../Company/Articles/Articles';
-import Sentiments from '../Company/Sentiments/Sentiments';
-import CompanySnippets from '../Company/CompanySnippets/CompanySnippets';
-import LineChart from '../Company/LineChart/LineChart';
-import TopTen from '../Company/TopTen/TopTen';
+const useStyles = makeStyles((theme) => ({
+ 
+  communityContainer: {
+    margin: '20px',
+    
+  }
+}));
 
-const useStyles = makeStyles({
-    root: {
-      minWidth: 275,
-      marginTop: '100px',
-    },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
-    carousel: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: '90vw',
-      height: '350px',
-      margin: '20px',
-      padding: '10px',
-      borderRadius: '5px',
-      boxShadow: '2px 5px 10px 5px rgba(0, 0, 0, 0.2)',
-    },
-    chart: {
-      display: 'flex',
-      justifyContent: 'center',
-    },
-    topten: {
-      display: 'flex',
-      margin: '20px',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+const Company = () => {
 
-function Company (){
-    const classes = useStyles();
-    const bull = <span className={classes.bullet}>•</span>;
+  const classes = useStyles();
 
-    return(
-        <>
-    <Card className={classes.root} variant="outlined">
-      <CardContent>
-        <Typography variant="h2" component="h2">
-        Amazon.com, Inc. (AMZN) +22.80 (+.32%) 1.61T 60.72
-        </Typography>
-      </CardContent>
-    </Card>
-        <div className={classes.chart}>
-        <LineChart />
-        </div>
-        <div className={classes.sentiments}>
-        <Sentiments />
-        </div>
-        <div className={classes.topten}>
-          <TopTen />
-        </div>
-        <div className={classes.carousel}>
-        <CompanySnippets />
-        </div>
-        <div className={classes.articles}>
-        <Articles />
-        </div>
-        </>
-    )
+  return (
+    <div className={classes.communityContainer}>
+      <Overview />
+    </div>
+  )
 }
 
-export default Company
+export default Company;
---
+++ src/components/Company/Overview/Overview.jsx
@@ -0,0 +1,128 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+
+import Charts from  '../Charts/Charts';
+import TopTen from '../TopTen/TopTen';
+import CompanySnippets from '../CompanySnippets/CompanySnippets';
+import Articles from '../Articles/Articles';
+
+
+
+const useStyles = makeStyles((theme) => ({
+ 
+  overview: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    marginTop: '80px'
+    
+    
+  }, 
+  carousel: {
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90vw',
+    height: '350px',
+    margin: '20px 0px 20px 0px',
+    padding: '10px',
+    borderRadius: '5px',
+    
+    
+    
+  },
+  CarouselContainer: {
+    position: 'relative',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    boxShadow: '2px 5px 10px 5px rgba(0, 0, 0, 0.2)',
+    margin: '20px 0px 20px 0px',
+    borderRadius: '5px',
+  },
+  chartMovers: {
+    display: 'flex',
+    borderRadius: '5px',
+    boxShadow: '2px 5px 10px 5px rgba(0, 0, 0, 0.2)',
+  },
+  movers: {
+    display:'flex',
+    flexGrow: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  bubbleChartContainer: {
+    display: 'flex',
+    flexGrow: 3,
+    margin: '0px 20px 0px 20px',
+    border: '1px solid white',
+    borderRadius: '4px',
+  },
+  TopTenContainer: {
+    display:'flex',
+    flexGrow: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  middle: {
+    display:'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    
+  },
+  chartAndTen: {
+    display:'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    
+  },
+  snippets: {
+    display:'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  carouselHeaders: {
+    padding: '20px',
+    margin: '10px',
+    color: 'gray',
+    
+  }
+}));
+
+
+const Overview = () => {
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.overview}>
+      
+
+        <div className={classes.chartAndTen}>
+        <div className={classes.chartMovers}>
+          <div className={classes.middle}>
+            <div className={classes.bubbleChartContainer}>
+              <Charts />
+            </div>
+          </div>
+          <div className={classes.TopTenContainer}>
+            {/* <TopTen /> */}
+          </div>
+        </div>
+        </div>
+        <div className={classes.CarouselContainer}>
+          <div className={classes.carousel}>
+            <h1 className={classes.carouselHeaders}>Check out the top trending tweets</h1>
+              <CompanySnippets />
+              <h1 className={classes.carouselHeaders}>Bookmark the article where the snippet originated</h1>
+          </div>
+        </div>
+        <Articles />
+    </div>
+    
+  )
+}
+
+export default Overview
