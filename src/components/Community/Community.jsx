defe15154556b9c6d941b2d6e0a7a3e4aecd951b
gabinwilliams <scoutsnknives@gmail.com>
2021-05-18 11:23:25 -0500

    fixed community layout with some css

--- src/components/Community/Community.jsx
+++ src/components/Community/Community.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
-
+import { makeStyles } from '@material-ui/core/styles';
 
 //import Tabs from '../Community/Tabs/Tabs';
 import Overview from '../Community/Overview/Overview';
 import News from '../Community/News/News';
+
+
+
+const useStyles = makeStyles((theme) => ({
+ 
+  communityContainer: {
+    margin: '20px',
+    
+  }
+}));
+
 const Community = () => {
+
+  const classes = useStyles();
+
   return (
-    <div>
-
-     
-
-
-      <header>
-        <h1>Community</h1>
-      </header>
-    <Overview />
-    <News />
+    <div className={classes.communityContainer}>
+      <Overview />
+      {/* <News /> */}
     </div>
   )
 }
