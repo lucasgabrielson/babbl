f8b29cfe7679cb3620ecce849308c3507f6ceb8c
gabinwilliams <scoutsnknives@gmail.com>
2021-05-19 10:30:58 -0500

    added snippet carousel to dashboard comp

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -5,6 +5,7 @@ import TickerCard from '../Dashboard/TickerCard/TickerCard';
 import WatchList from '../Dashboard/WatchList/WatchList';
 import BubbleChart from '../Dashboard/BubbleChart/BubbleChart';
 import Tabs from '../Dashboard/Tabs/Tabs';
+import SnippetsCarousel from '../Dashboard/SnippetsCarousel/SnippetsCarousel';
 
 
 const useStyles = makeStyles({
@@ -32,7 +33,38 @@ const useStyles = makeStyles({
     // flexWrap: 'wrap',
     flexDirection: 'column',
     alignItems: 'center',
-  }
+  },
+  CarouselContainer: {
+    position: 'relative',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    boxShadow: '2px 5px 10px 5px rgba(0, 0, 0, 0.2)',
+    margin: '20px 0px 20px 0px',
+    borderRadius: '5px',
+    // border: '2px solid black',
+    // flexWrap: 'nowrap',
+  },
+  carousel: {
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '350px',
+    margin: '20px 0px 20px 0px',
+    padding: '10px',
+    borderRadius: '5px',
+    // boxShadow: '2px 5px 10px 5px rgba(0, 0, 0, 0.2)',
+    
+  },
+  carouselHeaders: {
+    padding: '20px',
+    margin: '10px',
+    color: 'gray',
+    
+  },
   
 });
 
@@ -54,6 +86,14 @@ const Dashboard = () => {
           <WatchList />
         </div>
       </div>
+      <div className={classes.CarouselContainer}>
+      
+        <div className={classes.carousel}>
+        <h3 className={classes.carouselHeaders}>Check out the top trending tweets</h3>
+          <SnippetsCarousel />
+        </div>
+      </div>
+
     </div>
   )
 }
