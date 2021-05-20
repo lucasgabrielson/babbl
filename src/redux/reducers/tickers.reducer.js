90106069e204e330d191d363e1f6e237fe062063
lucasgabrielson <lucas.c.gabrielson@gmail.com>
2021-05-20 10:36:55 -0500

    percent change, points change, current price being returned from tickers reducer

--- src/redux/reducers/tickers.reducer.js
+++ src/redux/reducers/tickers.reducer.js
@@ -1,7 +1,11 @@
 const tickers = (state = [], action) => {
     switch (action.type) {
         case 'SET_TICKERS':
-            return action.payload;
+            return {
+                current_price: action.payload[action.payload.length - 1].close
+                percent_change: (action.payload[0].open - action.payload[action.payload.length - 1].close) / action.payload[0].opem * 100
+                points_change: action.payload[0].open - action.payload[action.payload.length - 1].close
+            };
 
         default:
             return state;
