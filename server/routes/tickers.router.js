ea80739c5d8f698dae8458f32f3ded8e6535c2c7
lucasgabrielson <lucas.c.gabrielson@gmail.com>
2021-05-20 10:40:36 -0500

    ticker information for price and changes in reducer

--- server/routes/tickers.router.js
+++ server/routes/tickers.router.js
@@ -7,7 +7,12 @@ router.get('/:ticker', (req, res) => {
     console.log( 'in GET /api/tickers');
         axios.get(`https://cloud.iexapis.com/stable/stock/${req.params.ticker}/intraday-prices/?token=${process.env.IEX_CLOUD_API_TOKEN}`)
             .then( response => {
-                res.send(response.data)
+                const objectToSend = {
+                    current_price: response.data[response.data.length - 1].close,
+                    percent_change: (response.data[0].open - response.data[response.data.length - 1].close) / response.data[0].open * 100,
+                    points_change: response.data[0].open - response.data[response.data.length - 1].close
+                }
+                res.send(objectToSend)
             }).catch( err => {
                 console.log( 'erroing connecting with babbl api /tickers GET');
                 res.sendStatus(500);
--- src/redux/reducers/tickers.reducer.js
+++ src/redux/reducers/tickers.reducer.js
@@ -1,11 +1,7 @@
 const tickers = (state = [], action) => {
     switch (action.type) {
         case 'SET_TICKERS':
-            return {
-                current_price: action.payload[action.payload.length - 1].close
-                percent_change: (action.payload[0].open - action.payload[action.payload.length - 1].close) / action.payload[0].opem * 100
-                points_change: action.payload[0].open - action.payload[action.payload.length - 1].close
-            };
+            return action.payload;
 
         default:
             return state;
