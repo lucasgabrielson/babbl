bbfa94d8dad4e56ec0283cd8e49079fb610da2ef
gabinwilliams <scoutsnknives@gmail.com>
2021-05-21 09:27:32 -0500

    updated top ten

--- src/components/Community/Overview/TopTen/TopTen.jsx
+++ src/components/Community/Overview/TopTen/TopTen.jsx
@@ -1,40 +1,47 @@
 import * as React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { DataGrid } from '@material-ui/data-grid';
 // import { useDemoData } from '@material-ui/x-grid-data-generator';
 
 export default function FlexLayoutGrid() {
-  // const { data } = useDemoData({
-  //   dataSet: 'Commodity',
-  //   rowLength: 10,
-  //   maxColumns: 4,
-  // });
+  
+  const populated_tickers = useSelector((store) => store.populated_tickers);
 
   const columns = [
-    { field: 'Ticker', headerName: 'Ticker'},
-    { field: 'Bull', headerName: 'Bull'},
-    { field: 'Bear', headerName: 'Bear'},
+    { field: 'Ticker', headerName: 'Ticker', width: 100},
+    { field: 'Price', headerName: 'Price', width: 100},
+    { field: 'Mentions', headerName: 'Mentions', width: 110},
     
  
   ];
   
   const rows = [
-    { id: 1, Ticker: 'TESLA', Bull: '+0.45', Bear: '-0.55'},
-    { id: 2, Ticker: 'APPL', Bull: '+0.45', Bear: '-0.55' },
-    { id: 3, Ticker: 'DIS', Bull: '+0.45', Bear: '-0.55' },
-    { id: 4, Ticker: 'LYFT', Bull: '+0.45', Bear: '-0.55' },
-    { id: 5, Ticker: 'LYFT', Bull: '+0.45', Bear: '-0.55' },
-    { id: 6, Ticker: 'LYFT', Bull: '+0.45', Bear: '-0.55' },
-    { id: 7, Ticker: 'LYFT', Bull: '+0.45', Bear: '-0.55' },
-    { id: 8, Ticker: 'LYFT', Bull: '+0.45', Bear: '-0.55' },
-    { id: 9, Ticker: 'LYFT', Bull: '+0.45', Bear: '-0.55' },
-    { id: 10, Ticker: 'LYFT', Bull: '+0.45', Bear: '-0.55' },
+
+
+    // { id: 1, 
+    //   Ticker: `${populated_tickers.data[0].ticker}`,
+
+    //   Mentions: `${populated_tickers.data[0].mentions}`},
+    { id: 1, Ticker: 'APPL', Price: '$75', Mentions: '-0.55' },
+    { id: 2, Ticker: 'APPL', Price: '$75', Mentions: '-0.55' },
+    { id: 3, Ticker: 'DIS', Price: '$30', Mentions: '-0.55' },
+    { id: 4, Ticker: 'LYFT', Price: '$120', Mentions: '-0.55' },
+    { id: 5, Ticker: 'LYFT', Price: '$120', Mentions: '-0.55' },
+    { id: 6, Ticker: 'LYFT', Price: '$120', Mentions: '-0.55' },
+    { id: 7, Ticker: 'LYFT', Price: '$120', Mentions: '-0.55' },
+    { id: 8, Ticker: 'LYFT', Price: '$120', Mentions: '-0.55' },
+    { id: 9, Ticker: 'LYFT', Price: '$120', Mentions: '-0.55' },
+    { id: 10, Ticker: 'LYFT', Price: '$120', Mentions: '-0.55' },
   ];
+
+  
+  
 
   return (
 
     <div style={{ height: '503px', width: '320px', }}>
       <div style={{ display: 'flex', flexDirection: 'column',  height: '100%', }}>
-        <div style={{ flexGrow: 1 }}>
+        <div style={{ flexGrow: 1, }}>
           <DataGrid rows={rows} columns={columns} pageSize={10}  />
         </div>
       </div>
