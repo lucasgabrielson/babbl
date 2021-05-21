da559a608317206632aad7790e14695871381b22
Joel Eckerson <joelaeckerson@gmail.com>
2021-05-21 10:29:42 -0500

    about page populated

--- src/components/LandingPageBabbl/views/About3View.jsx
+++ src/components/LandingPageBabbl/views/About3View.jsx
@@ -80,12 +80,23 @@ class About3View extends React.Component {
                       </div>
                     </div>
                   </div>
-                  <div className="af-class-empty-state w-dyn-empty">
+                  {/* <div className="af-class-empty-state w-dyn-empty">
                     <div>No items found.</div>
-                  </div>
+                  </div> */}
                 </div>
                 <div className="af-class-top-line-block">
                   <p className="af-class-paragraph">Babbl is a technology company that builds sentiment analysis tools for retail investors. Stock &amp; crypto traders young, old, novice and adept can use our software to improve their research process and gauge the mood of the markets more effectively. Though we are just getting started, our long-term goal is to be the one-stop-shop for all of your investment media analysis.</p>
+                  <p className="af-class-paragraph">Babbl was founded by Sam Cartford and Ramsey Shaffer in April 2020 with one straightforward idea: can we automate online conversation for the stock market? 
+                  With the market in a downward spiral brought on by international COVID-19 pandemic, we were itching to build something to help investors like us make sense of all of the news we were reading. Sam and Ramsey graduated from the University of Minnesota in May 2020 (Sam has a B.S. in Computer Science, Ramsey a B.S. in Industrial and Systems Engineering). 
+                  Sam currently works as a Technical Analyst for Boom Lab, and Ramsey will complete his M.S. in Data Analytics at the UMN in May 2021. Both Sam and Ramsey have 8-years combined experience in data science, fin-tech, cloud systems, and corpus linguistics, with primary experience in the technical analytics realm with companies like Inspire Brands, Boom Lab, and Wells Fargo. 
+                  While Sam's past work has involved computer science applications including cybersecurity, cloud platform development, and database management, Ramsey's past work involves process engineering applications such as big data analytics for fin-tech. Together, Sam and Ramsey were involved as undergraduates in the UMN's Carlson School of Management Algorithmic Trading club, 
+                  and co-authored a research paper in corpus linguistics about the relationship between social media chatter and cryptocurrency price performance (namely Bitcoin), which Ramsey presented at the 2019 International Corpus Linguistics Conference in Cardiff, UK. Since finishing their undergrad, Sam and Ramsey have committed their focus on making Babbl a profitable social-impact start-up. 
+                  Sam and Ramsey have leveraged lean start-up mentorship from Justin Grammens (co-founder of Lab651) and Matt Geiser (co-founder of Tin Roof Collective), and have received enduring start-up guidance and advice from Marshall Erickson (business accelerator and execution leader with the Red Wing Ignite entrepreneurship program) on a near-weekly basis since October 2020.</p>
+                  <p className="af-class-paragraph">In recent months, we have taken Babbl from an idea to a full-fledged pilot MVP website and browser extension. Since then, we have added Jeff Arnold to our team to help us iterate and improve upon our development. Jeff also graduated from the UMN in 2020, and is currently finishing up a graduate degree in Sports and Business Marketing. 
+                  As group, we have laid the ground work to make 2021 a big year. With suitable funding, Babbl's business model will continue to validate our problem-solution-customer fit by reaching more early adopters. We will pay a UI/UX development firm to complete professional MVP web- and mobile-applications for formal beta launch by July 2021. With our current pricing strategy, 
+                  we would out-pace our monthly burn rate ($500-$1500/month) with approximately 30 paying users. In parallel to MVP production, funding will allow us to work with a social marketing firm to develop our promotional strategy and reach our goal of 1,000+ early adopters by August 2021. With more user traction, we can affirm initial revenue streams, and begin to add more finance and data science expertise to our team by late 2021. 
+                  This will prepare Babbl for the release of fully market-ready web and mobile-applications (complete with usage-based subscription capabilities and machine learning models to improve the efficacy/accuracy of our research tools) by early 2022 at the latest, and allow us to engage and capture matching funding. If you made it this far reading through our About page, 
+                  thanks for being a part of this journey, and we are looking for all the feedback we can get to make this thing truly valuable to retail investors.</p>
                 </div>
               </div>
             </div>
