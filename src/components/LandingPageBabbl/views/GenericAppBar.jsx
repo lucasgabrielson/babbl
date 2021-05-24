9cae87d435255094b5e2fd0d82eedd27dd9853e5
lucasgabrielson <lucas.c.gabrielson@gmail.com>
2021-05-24 10:57:17 -0500

    autocomplete in navbars, styling less than desired

--- src/components/LandingPageBabbl/views/GenericAppBar.jsx
+++ src/components/LandingPageBabbl/views/GenericAppBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { createScope, map, transformProxies } from './helpers'
+import { createScope, map, transformProxies } from './helpers';
+import AutoComplete from '../../AppBar/AutoComplete';
 
 const scripts = [
   { loading: fetch("https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=609885bf0408da8300c9e62e").then(body => body.text()), isAsync: false },
@@ -50,6 +51,7 @@ class GenericAppBar extends React.Component {
               <div className="w-container">
                 <a href="index.html" className="af-class-logo-block w-nav-brand"><img src="images/finalbabbllogo-07.png" alt className="af-class-logo" /></a>
                 <nav role="navigation" className="af-class-nav-menu w-nav-menu">
+                  <span className="af-class-nav-link af-class-light"><AutoComplete /></span>
                   <a href="#/dashboard" className="af-class-nav-link af-class-light">Dashboard</a>
                   <a href="#/community" className="af-class-nav-link af-class-light">Community</a>
                   <a href={`#/bookmarks/${this.props.user.id}`} className="af-class-nav-link af-class-light">Bookmarks</a>
