3ae7fffc2b52d37d59eae5f04f6862479ee1ce8d
lucasgabrielson <lucas.c.gabrielson@gmail.com>
2021-05-24 09:56:23 -0500

    added correct names

--- src/components/LandingPageBabbl/views/CommunityAppBar.jsx
+++ src/components/LandingPageBabbl/views/CommunityAppBar.jsx
@@ -80,4 +80,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(CompanyAppBar);
+export default connect(mapStateToProps)(CommmunityAppBar);
--- src/components/LandingPageBabbl/views/DashboardAppBar.jsx
+++ src/components/LandingPageBabbl/views/DashboardAppBar.jsx
@@ -81,4 +81,4 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(CompanyAppBar);
+export default connect(mapStateToProps)(DashboardAppBar);
