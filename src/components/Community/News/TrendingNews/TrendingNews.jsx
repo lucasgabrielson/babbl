45a0696e2f106a62871b9309257d253bdb122a77
gabinwilliams <scoutsnknives@gmail.com>
2021-05-25 11:55:40 -0500

    updated trending news articles

--- src/components/Community/News/TrendingNews/TrendingNews.jsx
+++ src/components/Community/News/TrendingNews/TrendingNews.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { DataGrid } from '@material-ui/data-grid';
 import Button from '@material-ui/core/Button';
 
 const columns = [
   { field: 'date', headerName: 'Date', width: 150 },
+  { field: 'ticker', headerName: 'Ticker', width: 150 },
   { field: 'title', headerName: 'Title',  width: 300},
-  { field: 'source', headerName: 'Source', width: 150 },
   { field: 'mentions', headerName: 'Mentions', width: 300 },
   { field: 'score', headerName: 'Babbl Score', width: 150 },
+  {
+    field: "url",
+    headerName: "View Article",
+    disableClickEventBubbling: true,
+    renderCell: (params) => {
+    const onClick = () => {
+        console.log('url', params.row);
+    };
+    return <Button variant="contained" size="small" onClick={onClick}>View</Button>;
+    },
+    width: 175
+},
     {
       field: "bookmark",
       headerName: "Bookmark Article",
@@ -23,15 +36,34 @@ const columns = [
  
   ];
   
-  const rows = [
-    { id: 1, date: 'May 12, 2021', source: 'Yahoo', title: '"Stonks are going up!"', mentions: 'AMZN, GME', score: '< - 0.45' },
-    { id: 2, date: 'May 12, 2021', source: 'Yahoo', title: '"Stonks are going up!"', mentions: 'AMZN, GME', score: '< - 0.45'  },
-    { id: 3, date: 'May 12, 2021', source: 'Yahoo', title: '"Stonks are going up!"', mentions: 'AMZN, GME', score: '< - 0.45'  },
-    { id: 5, date: 'May 12, 2021', source: 'Yahoo', title: '"Stonks are going up!"', mentions: 'AMZN, GME', score: '< - 0.45'  },
-  ];
+  
 
 function TrendingNews(){
 
+  const dispatch = useDispatch();
+  
+  const babbl_api_articles = useSelector( store => store.babbl_api_articles);
+
+  const AAPL_articles = babbl_api_articles.AAPL;
+  const AMC_articles = babbl_api_articles.AMC;
+  const AMD_articles = babbl_api_articles.AMD;
+
+  const rows = [
+    //3 most recent articles from AAPL
+    { id: 1, date: `${AAPL_articles[0].timestamp}`, ticker: `APPL`, url: `${AAPL_articles[0].url}`, title: `"${AAPL_articles[0].title}"`, mentions: `${AAPL_articles[0].entities}`, score: `${Number(AAPL_articles[0].optimism - AAPL_articles[0].pessimism).toFixed(2)}` },
+    { id: 2, date: `${AAPL_articles[1].timestamp}`, ticker: `APPL`, url: `${AAPL_articles[1].url}`, title: `"${AAPL_articles[1].title}"`, mentions: `${AAPL_articles[1].entities}`, score: `${Number(AAPL_articles[1].optimism - AAPL_articles[1].pessimism).toFixed(2)}` },
+    { id: 3, date: `${AAPL_articles[2].timestamp}`, ticker: `APPL`, url: `${AAPL_articles[2].url}`, title: `"${AAPL_articles[2].title}"`, mentions: `${AAPL_articles[2].entities}`, score: `${Number(AAPL_articles[2].optimism - AAPL_articles[2].pessimism).toFixed(2)}` },
+
+    //3 most recent articles from AMC
+    { id: 4, date: `${AMC_articles[0].timestamp}`, ticker: `AMC`, url: `${AMC_articles[0].url}`, title: `"${AMC_articles[0].title}"`, mentions: `${AMC_articles[0].entities}`, score: `${Number(AMC_articles[0].optimism - AMC_articles[0].pessimism).toFixed(2)}` },
+    { id: 5, date: `${AMC_articles[1].timestamp}`, ticker: `AMC`, url: `${AMC_articles[1].url}`, title: `"${AMC_articles[1].title}"`, mentions: `${AMC_articles[1].entities}`, score: `${Number(AMC_articles[1].optimism - AMC_articles[1].pessimism).toFixed(2)}` },
+    { id: 6, date: `${AMC_articles[2].timestamp}`, ticker: `AMC`, url: `${AMC_articles[2].url}`, title: `"${AMC_articles[2].title}"`, mentions: `${AMC_articles[2].entities}`, score: `${Number(AMC_articles[2].optimism - AMC_articles[2].pessimism).toFixed(2)}` },
+
+    { id: 7, date: `${AMD_articles[0].timestamp}`, ticker: `AMD`, url: `${AMD_articles[0].url}`, title: `"${AMD_articles[0].title}"`, mentions: `${AMD_articles[0].entities}`, score: `${Number(AMD_articles[0].optimism - AMD_articles[0].pessimism).toFixed(2)}` },
+    { id: 8, date: `${AMD_articles[1].timestamp}`, ticker: `AMD`, url: `${AMD_articles[1].url}`, title: `"${AMD_articles[1].title}"`, mentions: `${AMD_articles[1].entities}`, score: `${Number(AMD_articles[1].optimism - AMD_articles[1].pessimism).toFixed(2)}` },
+    { id: 9, date: `${AMD_articles[2].timestamp}`, ticker: `AMD`, url: `${AMD_articles[2].url}`, title: `"${AMD_articles[2].title}"`, mentions: `${AMD_articles[2].entities}`, score: `${Number(AMD_articles[2].optimism - AMD_articles[2].pessimism).toFixed(2)}` },
+   
+  ];
 
     return(
         <>
