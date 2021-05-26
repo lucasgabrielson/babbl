4d2aba16263478be9a1fa759a48de705ba2f0c12
Trace Brandt <trpbr92@gmail.com>
2021-05-26 10:13:57 -0500

    working on bookmark post

--- src/components/Community/News/TrendingNews/TrendingNews.jsx
+++ src/components/Community/News/TrendingNews/TrendingNews.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { DataGrid } from '@material-ui/data-grid';
 import Button from '@material-ui/core/Button';
+import {useDispatch, useSelector} from 'react-redux';
 
 const columns = [
   { field: 'date', headerName: 'Date', width: 150 },
@@ -13,10 +14,22 @@ const columns = [
       headerName: "Bookmark Article",
       disableClickEventBubbling: true,
       renderCell: (params) => {
-      const onClick = () => {
+        const dispatch = useDispatch();
+        
+        const bookmark = (row) => {
           console.log('bookmark', params.row);
+          row = {
+            userID: params.userID,
+            date: params.row.date,
+            title: params.row.title,
+            source: params.row.source,
+            mentions: params.row.mentions,
+            link: params.row.link
+          }
+          dispatch({ type: 'ADD_USER_ARTICLES', payload: row});
       };
-      return <Button variant="contained" size="small" onClick={onClick}>bookmark</Button>;
+      
+      return <Button variant="contained" size="small" onClick={()=>bookmark(params.row)}>bookmark</Button>;
       },
       width: 175
   },
@@ -24,12 +37,11 @@ const columns = [
   ];
   
   const rows = [
-    { id: 1, date: 'May 12, 2021', source: 'Yahoo', title: '"Stonks are going up!"', mentions: 'AMZN, GME', score: '< - 0.45' },
-    { id: 2, date: 'May 12, 2021', source: 'Yahoo', title: '"Stonks are going up!"', mentions: 'AMZN, GME', score: '< - 0.45'  },
-    { id: 3, date: 'May 12, 2021', source: 'Yahoo', title: '"Stonks are going up!"', mentions: 'AMZN, GME', score: '< - 0.45'  },
-    { id: 5, date: 'May 12, 2021', source: 'Yahoo', title: '"Stonks are going up!"', mentions: 'AMZN, GME', score: '< - 0.45'  },
+    { id: 1, date: '2021-05-26', source: 'Yahoo', title: '"Stonks!"', mentions: 'AMZN, GME', score: '< - 0.45' },
+    { id: 2, date: 'May 13, 2021', source: 'Google', title: '"Stonks are!"', mentions: 'AMZN, GME', score: '< - 0.45'  },
+    { id: 3, date: 'May 14, 2021', source: 'Robinhood', title: '"Stonks are going!"', mentions: 'AMZN, GME', score: '< - 0.45'  },
+    { id: 5, date: 'May 15, 2021', source: 'Yahoo', title: '"Stonks are going up!"', mentions: 'AMZN, GME', score: '< - 0.45'  },
   ];
-
 function TrendingNews(){
 
 
