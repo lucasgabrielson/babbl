278cc6fe8d0c786826a250ebafb3e84034bfa625
lucasgabrielson <lucas.c.gabrielson@gmail.com>
2021-05-26 10:59:30 -0500

    company line chart up and running

--- src/components/Company/Charts/LineChart/LineChart.jsx
+++ src/components/Company/Charts/LineChart/LineChart.jsx
@@ -9,59 +9,141 @@ function LineChart (){
   const dispatch = useDispatch();
   const params = useParams();
   const times = useSelector((store)=> {return store.timeseries_sentiments})
-  const prices = useSelector((store) => {return store.timeseries_prices})
+  const prices = useSelector((store) => {return store.timeseries_prices});
+  const ticker = params.id
 
   useEffect(()=> {
     dispatch({ type: 'FETCH_TIMESERIES_SENTIMENTS', payload: {tickers: params.id, days: 7} })
-    dispatch({ type: 'FETCH_TIMESERIES_PRICES'})
+    dispatch({ type: 'FETCH_TIMESERIES_PRICES', payload: {ticker: params.id, days: '5d'} })
   }, []);
   console.log('params:', params);
 
+
+  let array = [];
+
+  if( times[0] !== undefined && prices[0] !== undefined ) {
+    for( let i = 0; i < times[0][ticker].length; i++ ) {
+      console.log(times[0][ticker][i].date.slice(0,4), 
+        times[0][ticker][i].date.slice(5,7), 
+        times[0][ticker][i].date.slice(8,10));
+      array.push(
+        [
+          new Date(
+            times[0][ticker][i].date.slice(0,4), 
+            times[0][ticker][i].date.slice(5,7) - 1, 
+            times[0][ticker][i].date.slice(8,10)
+          ),
+          times[0][ticker][i].value,
+          prices[i]["close"]
+        ]
+      )
+    }
+  }
+
+  
     return(
         <>
-        {JSON.stringify(params.id)}
-
-        {/* {times[0].params.id.map(time => {
-          return (
-            <div>
-              <p>{time.date}</p>
-              <p>{time.value}</p>
-
-            </div>
-          )
-        })} */}
-
+        {/* {JSON.stringify(params.id)}
+        {JSON.stringify(times)} */}
+        {/* {JSON.stringify(prices)} */}
+        {JSON.stringify(array)}
+        {/* { array.length &&  */}
         <Chart
-  width={'1200px'}
-  height={'600px'}
-  chartType="LineChart"
-  loader={<div>Loading Chart</div>}
-  data={[
-    ['x', 'Sentiments', 'Prices'],
-    [0, 0, 0],
-    [1, 10, 30],
-    [2, 23, 45],
-    [3, 17, 50],
-    [4, 18, 2],
-    [5, 9, 24],
-    [6, 11, 34],
-    [7, 27, 77],
-  ]}
-  options={{
-    hAxis: {
-      title: 'Days',
-    },
-    vAxis: {
-      title: 'Change',
-    },
-    series: {
-      1: { curveType: 'function' },
-    },
-  }}
-  rootProps={{ 'data-testid': '2' }}
-/>
+          width={'100%'}
+          height={'500'}
+          chartType="Line"
+          loader={<div>Loading Chart . . .</div>}
+          data={[
+            [
+              { type: 'date', label: 'Day'},
+              'Babbl Score',
+              'Closing Price',
+            ],
+            ...array
+          ]}
+          options={{
+            chart: {
+              title:
+                'Daily Closing Price and Babbl Score',
+            },
+            width: 900,
+            height: 500,
+            series: {
+              // Gives each series an axis name that matches the Y-axis below.
+              0: { axis: 'Babbl Score' },
+              1: { axis: 'Price (USD)' },
+            },
+            axes: {
+              // Adds labels to each axis; they don't have to match the axis names.
+              y: {
+                Temps: { label: 'Dunno' },
+                Daylight: { label: 'Price (USD)' },
+              },
+            },
+          }}
+          rootProps={{ 'data-testid': '4' }}
+        />
+        {/* } */}
         </>
     )
 }
 
 export default LineChart;
+
+
+/**
+  const [sentiments, setSentiments] = React.useState(
+    [
+      {
+      date: '2021-05-19',
+      value: 5.8,
+      },
+      {
+      date: '2021-05-19',
+      value: 5.8,
+      },
+      {
+      date: '2021-05-19',
+      value: 5.8,
+      },
+    ]
+  );
+
+  const [stockPrices, setStockPrices] = React.useState(
+    [
+      {
+        "date" : '2021-05-19',
+        "close": 124.69
+      },
+      {
+        "date" : '2021-05-19',
+        "close": 124.69
+      },
+      {
+        "date" : '2021-05-19',
+        "close": 124.69
+      },
+    ]
+  )
+
+  React.useEffect(() => {
+    const data = localStorage.getItem("sentiments");
+    if (data) {
+      setSentiments(JSON.parse(data));
+    }
+  }, []);
+
+  React.useEffect(() => {
+    const data = localStorage.getItem("stockPrices");
+    if (data) {
+      setStockPrices(JSON.parse(data));
+    }
+  }, []);
+
+  React.useEffect(() => {
+    localStorage.setItem("sentiments", JSON.stringify(times));
+  });
+
+  React.useEffect(() => {
+    localStorage.setItem("stockPrices", JSON.stringify(prices));
+  }); */
