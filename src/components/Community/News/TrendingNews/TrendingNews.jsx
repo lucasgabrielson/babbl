28e3573bbb4de490db83f7a6819c9e66a8c27fb7
Trace Brandt <trpbr92@gmail.com>
2021-05-27 14:38:11 -0500

    bookmark button removed from community page

--- src/components/Community/News/TrendingNews/TrendingNews.jsx
+++ src/components/Community/News/TrendingNews/TrendingNews.jsx
@@ -27,34 +27,7 @@ const columns = [
     },
     width: 100
 },
-    {
-      field: "bookmark",
-      headerName: "Bookmark",
-      disableClickEventBubbling: true,
-      renderCell: (params) => {
-        const dispatch = useDispatch();
-        const userID = useSelector((store) => {return store.user});
-        
-        const bookmark = (row) => {
-          console.log('bookmark', params.row);
-          row = {
-            userID: userID.id,
-            date: params.row.date,
-            title: params.row.title,
-            ticker: params.row.ticker,
-            mentions: params.row.mentions,
-            link: params.row.link
-          }
-          dispatch({ type: 'ADD_USER_ARTICLES', payload: row});
-      };
-
-      return  <IconButton variant="contained" size="small" onClick={()=>bookmark(params.row)}>
-                <BookmarkBorderIcon />
-              </IconButton>
-
-      },
-      width: 100
-  },
+    
 
   ];
 
