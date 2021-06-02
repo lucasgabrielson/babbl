0a13d24b9c6447e67fc6c04a9cbb5931b140cc74
gabinwilliams <scoutsnknives@gmail.com>
2021-06-02 16:27:16 -0500

    cleaned up community and added comments

--- src/components/Community/Overview/TopMovers/TopMovers.jsx
+++ src/components/Community/Overview/TopMovers/TopMovers.jsx
@@ -1,34 +1,31 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    minWidth: '200px',
-    width: '200px',
-    marginBottom: '20px',
-    
-    boxShadow: '2px 2px 2px 2px rgba(0, 0, 0, 0.2)',
+    minWidth: "200px",
+    width: "200px",
+    marginBottom: "20px",
+
+    boxShadow: "2px 2px 2px 2px rgba(0, 0, 0, 0.2)",
   },
   bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
   },
   title: {
-    display: 'flex',
-    justifyContent: 'center',
-    color: 'black',
+    display: "flex",
+    justifyContent: "center",
+    color: "black",
     fontSize: 14,
-    marginBottom: '20px',
-    borderBottom: '5px solid gray',
-    borderRadius: '5px'
-    
+    marginBottom: "20px",
+    borderBottom: "5px solid gray",
+    borderRadius: "5px",
   },
   headers: {
     fontSize: 12,
@@ -46,40 +43,37 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.black.main,
   },
   ticker: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    // color: theme.primary,
-  }
+    display: "flex",
+    justifyContent: "space-between",
+  },
 }));
 
-
-
 export default function SimpleCard() {
-
   const classes = useStyles();
-  
+
   const top_movers = useSelector((store) => store.top_movers);
 
-  console.log('top_movers', top_movers.top);
+  console.log("top_movers", top_movers.top);
+  // initial dummy data state for top movers
 
   const [top, setTop] = React.useState([
-   
     {
-      ticker: 'AMD',
+      ticker: "AMD",
       movement: 5.8,
     },
     {
-      ticker: 'AAPL',
+      ticker: "AAPL",
       movement: 10,
     },
     {
-      ticker: 'TSLA',
+      ticker: "TSLA",
       movement: 8,
     },
-
   ]);
 
   React.useEffect(() => {
+    // looks for local storage data on load
+
     const data = localStorage.getItem("top_movers");
     if (data) {
       setTop(JSON.parse(data));
@@ -87,42 +81,49 @@ export default function SimpleCard() {
   }, []);
 
   React.useEffect(() => {
+    // stores bottom movers state in local storage so you can refresh and it will pull from storage
+
     localStorage.setItem("top_movers", JSON.stringify(top_movers.top));
   });
-
-
 
   return (
     <Card className={classes.root}>
       <CardContent>
-          <Typography className={classes.title} color="textSecondary" gutterBottom>
-            Top 3 Babbl Movers
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          Top 3 Babbl Movers
+        </Typography>
+        <div className={classes.ticker}>
+          <Typography
+            className={classes.headers}
+            color="textSecondary"
+            gutterBottom
+          >
+            Ticker
           </Typography>
+          <Typography
+            className={classes.headers}
+            color="textSecondary"
+            gutterBottom
+          >
+            Score Change
+          </Typography>
+        </div>
+        {top.map((company) => (
           <div className={classes.ticker}>
-            <Typography className={classes.headers} color="textSecondary" gutterBottom>
-              Ticker
+            <Typography className={classes.company}>
+              {company.ticker}
             </Typography>
-            <Typography className={classes.headers} color="textSecondary" gutterBottom>
-              Score Change
+
+            <Typography className={classes.pos}>
+              {company.movement.toFixed(2)}%
             </Typography>
           </div>
-        {top.map(company => ( 
-        <div className={classes.ticker}>
-        
-          <Typography className={classes.company} >
-            {company.ticker}
-          </Typography>
-
-          <Typography className={classes.pos} >
-            {(company.movement).toFixed(2)}%
-          </Typography>
-
-         
-
-        </div>
-          ))}  
+        ))}
       </CardContent>
-      
     </Card>
   );
 }
